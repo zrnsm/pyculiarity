ed0e62455a792cebdc19260900d2a1ad475bec1b
Nicolas Steven Miller <nicolasmiller@gmail.com>
2018-08-27 21:33:57 -0700

    update version

--- setup.py
+++ setup.py
@@ -16,7 +16,7 @@ from setuptools import setup, find_packages
 
 setup(
     name='pyculiarity',
-    version='0.0.6',
+    version='0.0.7',
     description='A Python port of Twitter\'s AnomalyDetection R Package.',
     long_description=__doc__,
     url='https://github.com/nicolasmiller/pyculiarity',
